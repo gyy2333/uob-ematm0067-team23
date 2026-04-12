3728e812e89911ee636ef77922a48f45b9576209
gujingting <jingting515@gmail.com>
2026-04-13 00:24:45 +0100

    🐞 fix(main): add arg parse
    1. Compatible with the previously submitted run_modeling.sh
    2. Set the input and output save paths through the command line.

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
+import argparse
 from Modeling.modeling import Modeling
 
 
 def main():
-    data_path = "data/modeling/ai_ml_nlp_dataset_processed.csv"
-    output_path = "outputs/"
-    model1 = Modeling.create("lda", data_path=data_path, output_path=output_path)
-    print(model1.speak())
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data-path",
+        type=str,
+        default="data/modeling/ai_ml_nlp_dataset_processed.csv",
+        help="The path of the file of preprocessed data",
+    )
+    parser.add_argument(
+        "--output-path",
+        type=str,
+        default="outputs/",
+        help="The directory saving output topic file and the figures",
+    )
+    args = parser.parse_args()
 
-    model2 = Modeling.create("bert", data_path=data_path, output_path=output_path)
-    print(model2.speak())
-    model2.process()
+    data_path = args.data_path
+    output_path = args.output_path
+
+    print("===input data path:", data_path)
+    print("===output data path:", output_path)
+
+    bert_model = Modeling.create("bert", data_path=data_path, output_path=output_path)
+    bert_model.process()
+
+    lda_model = Modeling.create("lda", data_path=data_path, output_path=output_path)
+    lda_model.process()
 
 
 if __name__ == "__main__":
